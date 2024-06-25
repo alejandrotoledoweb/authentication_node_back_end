5b1022bfb23fad2927721b096379da5ca2ce5fd4
Alejandro Toledo <atoledofr@gmail.com>
2024-06-25 15:33:26 -0500

    changes in the user model

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,62 @@
 // src/server.ts
 
 import express, { Request, Response } from 'express';
-import User from './models/User';
+import { User } from './models/User';
 import sequelize from './models';
+import { ValidationError } from 'sequelize';
 
 const app = express();
 const port = 3000;
 
-app.use(express.json())
+app.use(express.json());
 
 app.get('/', async (req: Request, res: Response) => {
   const users = await User.findAll();
   res.json(users);
 });
 
-sequelize.sync().then(() => {
-  app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-  });
-}).catch((err) => {
-  console.error('Unable to connect to the database:', err);
+app.post('/register', async (req: Request, res: Response) => {
+  try {
+    const user = await User.create(req.body);
+    console.log(user);
+    res.status(201).json(user);
+  } catch (error) {
+    console.log(error);
+    if (error instanceof ValidationError) {
+      res.status(400).json({
+        errors: error.errors.map((e: any) => e.message),
+        values: error.errors.map((e: any) => e.path),
+      });
+    } else {
+      res.status(402).json({ messaga: 'something went wrong!' });
+    }
+  }
 });
+app.post('/login', async (req: Request, res: Response) => {
+  try {
+    const user = await User.findOne(req.body);
+    console.log(user);
+    res.status(201).json(user);
+  } catch (error) {
+    console.log(error);
+    if (error instanceof ValidationError) {
+      res.status(400).json({
+        errors: error.errors.map((e: any) => e.message),
+        values: error.errors.map((e: any) => e.path),
+      });
+    } else {
+      res.status(402).json({ messaga: 'something went wrong!' });
+    }
+  }
+});
+
+sequelize
+  .sync({ alter: true })
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running at http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,52 +1,110 @@
-// src/models/User.ts
-
 import { DataTypes, Model, Optional } from 'sequelize';
 import sequelize from './index';
 
-interface UserAttributes {
-  id: number;
-  name: string;
-  email: string;
-  password: string;
-  createdAt?: Date;
-  updatedAt?: Date;
-}
-
-interface UserCreationAttributes extends Optional<UserAttributes, 'id'> {}
-
-class User extends Model<UserAttributes, UserCreationAttributes> implements UserAttributes {
-  public id!: number;
-  public name!: string;
-  public email!: string;
-  public password!: string;
-  public readonly createdAt!: Date;
-  public readonly updatedAt!: Date;
-}
-
-User.init(
-  {
-    id: {
-      type: DataTypes.INTEGER.UNSIGNED,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    name: {
-      type: new DataTypes.STRING(128),
-      allowNull: false,
-    },
-    email: {
-      type: new DataTypes.STRING(128),
-      allowNull: false,
-    },
-    password: {
-      type: new DataTypes.STRING(128),
-      allowNull: false,
+export const User = sequelize.define('user', {
+  email: {
+    type: DataTypes.STRING,
+    unique: true,
+    validate: {
+      notEmpty: {
+        msg: 'Email cannot be empty',
+      },
     },
   },
-  {
-    sequelize,
-    tableName: 'users',
-  }
-);
+  password: {
+    type: DataTypes.STRING,
+    unique: false,
+    validate: {
+      notEmpty: {
+        msg: 'Password cannot be empty',
+      },
+      len: {
+        args: [6, 50],
+        msg: 'Password must be between 3 and 50 characters',
+      },
+    },
+  },
+});
 
-export default User;
+// interface UserAttributes {
+//   id: number;
+//   name: string;
+//   username: string;
+//   email: string;
+//   password: string;
+//   createdAt?: Date;
+//   updatedAt?: Date;
+// }
+
+// interface UserCreationAttributes extends Optional<UserAttributes, 'id'> {}
+
+// class User
+//   extends Model<UserAttributes, UserCreationAttributes>
+//   implements UserAttributes
+// {
+//   public id!: number;
+//   public name!: string;
+//   public username!: string;
+//   public email!: string;
+//   public password!: string;
+//   public readonly createdAt!: Date;
+//   public readonly updatedAt!: Date;
+// }
+
+// User.init(
+//   {
+//     id: {
+//       type: DataTypes.INTEGER,
+//       autoIncrement: true,
+//       primaryKey: true,
+//     },
+//     name: {
+//       type: new DataTypes.STRING(128),
+//       allowNull: true,
+//       validate: {
+//         notEmpty: {
+//           msg: 'Name cannot be empty',
+//         },
+//       },
+//     },
+//     username: {
+//       type: new DataTypes.STRING(128),
+//       allowNull: false,
+//       unique: true,
+//       validate: {
+//         notEmpty: {
+//           msg: 'Username cannot be empty',
+//         },
+//       },
+//     },
+//     email: {
+//       type: new DataTypes.STRING(128),
+//       allowNull: false,
+//       unique: true,
+//       validate: {
+//         isEmail: {
+//           msg: 'Must be a valid email address',
+//         },
+//       },
+//     },
+//     password: {
+//       type: new DataTypes.STRING(128),
+//       allowNull: false,
+//       validate: {
+//         notEmpty: {
+//           msg: 'Name cannot be empty',
+//         },
+//         len: {
+//           args: [6, 50],
+//           msg: 'Password must be between 3 and 50 characters',
+//         },
+//       },
+//     },
+//   },
+//   {
+//     sequelize,
+//     tableName: 'users',
+//   }
+// );
+
+// export default User;
